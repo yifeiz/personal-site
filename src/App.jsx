84af93f8cddaf22cea913af7f17a27f680acd788
Yifei Zhang <yifei7.zhang@gmail.com>
2020-10-16 00:09:01 -0400

    some initial shenanigans

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import profile from "./favicon.jpg";
+
+import "./App.css";
 
 const App = () => {
-  return(<div>Hello!</div>)
-}
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div className="main-card">
+        <img className="profile-img" src={profile} alt="profile" />
+        <div style={{ color: "white" }}>
+          hey there! lorem ipsum, blah blah a;sdlfkjasd;flkjasd;flkajds;flkajs
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
