91369f8d9ecc1390546e071d1de9647e28aba0cb
Yifei Zhang <yifei7.zhang@gmail.com>
2021-01-13 15:52:26 -0500

    add updated resume + some nicer fade in on initial load

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import profile from "./imgs/headshot.jpg";
 import background from "./imgs/background.jpg";
@@ -7,23 +7,26 @@ import "./styles/App.scss";
 
 const App = () => {
   const [loaded, setLoaded] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  // TODO: Restart css animation (https://css-tricks.com/restart-css-animation/)
 
-  if (!loaded) {
-    return (
-      <div className="root">
-        <img
-          src={background}
-          style={{ display: "none" }}
-          onLoad={() => setLoaded(true)}
-          alt="temp-background"
-        />
-        BLAH
-      </div>
-    );
-  }
   return (
-    <div className="root">
-      <div className="main-card">
+    <div className={`home ${loaded ? `fade-in` : null}`}>
+      <img
+        src={background}
+        style={{ display: "none" }}
+        onLoad={() => setImageLoaded(true)}
+        alt="temp-background"
+      />
+      <div className="drop" />
+      <div
+        className="wave"
+        onAnimationEnd={() => {
+          setLoaded(true);
+        }}
+      />
+
+      <div className={`main-card ${loaded ? `fade-in` : null}`}>
         <img className="profile-img" src={profile} alt="profile" />
         <div className="card-text">
           Hey there! My name is Yifei Zhang, and I'm entering my 3rd year of
@@ -31,27 +34,27 @@ const App = () => {
           connect with me using any of the links below!
         </div>
       </div>
-      <div className="footer">
+      <div className={`footer ${loaded ? `fade-in` : null}`}>
         <a
           href="https://github.com/yifeiz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i class="fab fa-github fa-2x"></i>
+          <i className="fab fa-github fa-2x"></i>
         </a>
         <a
           href="https://www.linkedin.com/in/yifei-zhang1/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i class="fab fa-linkedin-in fa-2x"></i>
+          <i className="fab fa-linkedin-in fa-2x"></i>
         </a>
         <a
           href="mailto:yifei.zhang@uwaterloo.ca"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i class="far fa-envelope fa-2x"></i>
+          <i className="far fa-envelope fa-2x"></i>
         </a>
         <a
           href={`${process.env.PUBLIC_URL}/resume.pdf`}
